6dc3620cd125ea291acda05b1bb68e0c5bcf172d
Abraham Furlan <abrahamfurlan@gmail.com>
2022-11-13 17:07:32 -0500

    fixed a stupid mac line length eslint error >:(

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -20,7 +20,8 @@ const makeDomo = async (req, res) => {
   try {
     const newDomo = new Domo(domoData);
     await newDomo.save();
-    return res.status(201).json({ name: newDomo.name, age: newDomo.age, obsession: newDomo.obsession });
+    return res.status(201)
+      .json({ name: newDomo.name, age: newDomo.age, obsession: newDomo.obsession });
   } catch (err) {
     console.log(err);
     if (err.code === 11000) {
